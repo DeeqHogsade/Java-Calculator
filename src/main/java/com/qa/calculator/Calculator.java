aeb1f0d0226d8982cd1253b2b95243416f5330b8
deeg.hogsade <deeg.hogsade@academytrainee.com>
2019-05-29 12:39:21 +0100

    added user input functionality

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -2,8 +2,12 @@ package com.qa.calculator;
 
 public class Calculator {
 
-	public int sub(int num1, int num2) {
-		return num1 - num2;
+	
+
+	
+	public int sub(int num, int num2) {
+		return num - num2;
 	}
+	
 
 }
--- src/test/java/com/qa/calculatorTest/CalculatorTest.java
+++ src/test/java/com/qa/calculatorTest/CalculatorTest.java
@@ -6,19 +6,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.qa.calculator.Calculator;
+import com.qa.calculator.UserInput;
 
 public class CalculatorTest {
 	Calculator calc;
-	
+
 	@Before
-	public void setup(){
+	public void setup() {
 		calc = new Calculator();
 	}
-	
+
 	@Test
-	public void subtractNumbers() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.sub(5,3);
+	public void userInputTest() {
+		UserInput input = new UserInput();
+		int user = input.input();
+		System.out.println(user);
+		assertEquals("User input error", 2, user);
+	}
+
+	@Test
+	public void subtractNumbersTest() {
+		int actualValue = calc.sub(5, 3);
+		System.out.println(actualValue);
 		assertEquals("Subtraction error", 2, actualValue);
 	}
+
 }
