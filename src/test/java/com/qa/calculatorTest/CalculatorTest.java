6f37b3aa668d55d3a24b01e10880dfcc16d3b459
deeg.hogsade <deeg.hogsade@academytrainee.com>
2019-05-29 11:48:57 +0100

    added subtraction functionality

--- src/test/java/com/qa/calculatorTest/CalculatorTest.java
+++ src/test/java/com/qa/calculatorTest/CalculatorTest.java
@@ -1,17 +1,24 @@
 package com.qa.calculatorTest;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.qa.calculator.Calculator;
 
 public class CalculatorTest {
+	Calculator calc;
+	
+	@Before
+	public void setup(){
+		calc = new Calculator();
+	}
 	
 	@Test
 	public void subtractNumbers() {
 		Calculator calc = new Calculator();
-		calc.input();
-		int actualValue = calc.add(5,3);
+		int actualValue = calc.sub(5,3);
 		assertEquals("Subtraction error", 2, actualValue);
 	}
 }
